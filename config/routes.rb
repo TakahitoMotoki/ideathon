78e2b426fd0003f9c0936346242591444381e66e
元木嵩人 <kmnernofkingdeep@gmail.com>
2018-03-27 19:57:32 +0900

    FB Auth HTTPS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registration: "registration" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "hello#index"
 end
