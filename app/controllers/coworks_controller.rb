913ccbb1a9f31fbf6175b77d886849aec16d0ffa
元木嵩人 <kmnernofkingdeep@gmail.com>
2018-04-11 20:15:06 +0900

    Made controllers & models

---
+++ app/controllers/coworks_controller.rb
@@ -0,0 +1,2 @@
+class CoworksController < ApplicationController
+end
---
+++ test/controllers/coworks_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CoworksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
